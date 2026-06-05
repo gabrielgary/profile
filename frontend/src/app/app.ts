a2c0c4e3c547a2331ac127a129b637a4579bf09a
gabrielgary <gabrielpedroaurelio1@gmail.com>
2026-06-05 20:59:27 +1000

    file created

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Herosection } from './client/herosection/herosection';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Herosection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
